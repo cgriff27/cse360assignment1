39c828cb03b7e185275dd5dbc13c949c38ea93d5
cgriff27 <cgriff27@asu.edu>
2020-10-05 15:34:26 -0700

    Initial commit

---
+++ AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * Constructs an object of class AddingMachine
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Gets the total stored in the AddingMachine
+   * @return    the total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Adds the value to the total stored in the AddingMachine
+   * @param value   the value to add
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Subtracts the value from the total stored in the AddingMachine
+   * @param value   the value to subtract
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Returns a string containing a history of transactions
+   * @return    transaction history
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Clears the transaction history
+   */
+  public void clear() {
+  }
+}
